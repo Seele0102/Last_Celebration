6b2c498c125ffa997e8dec7f1ee150a215bcc899
希儿单推人 <hh75624777163@163.com>
2023-02-26 16:03:20 +0800

    远程怪

--- Assets/Scripts/Enemy/Enemy_3.cs
+++ Assets/Scripts/Enemy/Enemy_3.cs
@@ -1,31 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Enemy_3 : MonoBehaviour
 {
-    private GameObject Player;
-    private Vector3 Direction;
-    public float Health, Attack, Defence, Speed;
+    public GameObject Player,Bullet;
+    public float Distance;
+    public Vector3 Direction;
+    public float Speed, Health, Attack, Defence;
+    private float AttackTimer;
     private void Start()
     {
-        Player = GameObject.FindGameObjectWithTag("Player");
-        Health = 200 * GameManager.HealthRise;
-        Attack = 5 * GameManager.AttRise;
-        Defence = 50 * GameManager.DefRise;
+        AttackTimer = 5;
     }
     private void Update()
     {
-        Direction = gameObject.transform.position - Player.transform.position;
-        Direction = Direction.normalized;
-        MoveLogic();
+        Prepare();
         if (Health <= 0)
         {
             Destroy(gameObject);
         }
+        Move();
+        AttackBehavior();
+        if(AttackTimer> 0)
+        {
+            AttackTimer-= Time.deltaTime;
+        }
     }
-    private void MoveLogic()
+    private void Prepare()
     {
-        gameObject.transform.position += Direction * Speed;
+        Speed = 1;
+        Health = 100*GameManager.HealthRise;
+        Attack = 10*GameManager.AttRise;
+        Defence = 10*GameManager.DefRise;
+        Direction=Player.transform.position-gameObject.transform.position;
+        Distance=Vector3.Distance(Direction,Vector3.zero);
+        Direction=Direction.normalized;
+    }
+    private void Move()
+    {
+        if(Distance<=2)
+        {
+            gameObject.transform.position -= Speed * Time.deltaTime * Direction;
+        }
+        else if(Distance>=2.2f)
+        {
+            gameObject.transform.position += Speed * Time.deltaTime * Direction;
+        }
+    }
+    private void AttackBehavior()
+    {
+        if (AttackTimer <= 0)
+        {
+            AttackTimer = 5;
+            GameObject.Instantiate(Bullet);
+        }
     }
 }
